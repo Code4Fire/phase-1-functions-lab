569efac0319fea7c19559010234dbd59057e4acc
Tiffany_Tam <tamtiffany89@gmail.com>
2021-10-13 15:20:02 -0400

    commit tree 4dffe957345f11b4a3308a5869f845fe75eb3e70

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const hQLocation = 42;
+
+function distanceFromHqInBlocks(pickUpLocation) {
+   return Math.abs(hQLocation- pickUpLocation)
+}
+
+
+function distanceFromHqInFeet (pickUpLocation) {
+    return distanceFromHqInBlocks(pickUpLocation) * 264;
+}
+
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice (start, end) {
+    let distance = distanceTravelledInFeet(start, end);
+    
+    if (distance >= 400 && distance <= 2000) {
+        return Math.abs(distance* 0.02 - 8);
+    } else if (distance <= 400) {
+        return 0;
+    } else if 
+        (distance >= 2000 && distance < 2500) {
+            return 25;
+    }else {
+        return 'cannot travel that far'
+    }
+
+}
